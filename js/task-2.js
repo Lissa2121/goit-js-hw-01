3aaf8960b440ff0fd00f867d8a3a94a7a17cb04d
Lissa2121 <114819609+Lissa2121@users.noreply.github.com>
2024-04-05 17:11:01 +0300

    hw1

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,3 @@
-// function getElementWidth(content, padding, border) {
-//     return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-// }
-
-// const content = getElementWidth("50px", "8px", "4px");
-// const padding = getElementWidth("60px", "12px", "8.5px");
-// const border = getElementWidth("200px", "0px", "0px");
-
-// console.log(content);
-// console.log(padding);
-// console.log(border);
 
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
